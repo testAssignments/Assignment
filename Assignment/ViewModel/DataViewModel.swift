daee4e2b0a1d79dc0ca3eefcb702a8ba99daff19
Tushar Dalvi <tushardalvig@gmail.com>
2019-07-18 09:50:53 +0530

    Updates

---
+++ Assignment/ViewModel/DataViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataViewModel.swift
+//  Assignment
+//
+//  Created by test on 18/07/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class DataViewModel: NSObject {
+    
+}
